669bdb47aa9e99e47603fd1ef2e7ec9d08b75583
Leto <leto-ziegler@web.de>
2021-07-28 20:15:09 +0200

    x

--- js/function.js
+++ js/function.js
@@ -1,10 +1,11 @@
 
 var my_home = document.querySelectorAll('.h1');
 var Titles = ["WELCOME TO MY SITE", "ABOUT ME", "MY SKILLS"];
-//var hamburger = document.querySelector(".hamburg");
+
 for(var i=0; i<my_home.length; ++i){
-    my_home[i].textContent = Titles[i];
+    if(i < Titles.length){
+        my_home[i].textContent = Titles[i];
+    }else{
+        my_home[i].textContent = "ERROR: Titles[] out of bound";
+    }
 }
-//   hamburger.onclick = function () {
-//      this.classList.toggle ("checked");
-//  }
