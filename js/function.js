c3782a533de919ff93612dd34d7d36d90d4120b0
Leto <leto-ziegler@web.de>
2021-08-05 11:07:05 +0200

    js empty, moved to html

--- js/function.js
+++ js/function.js
@@ -1,11 +1,1 @@
 
-var my_home = document.querySelectorAll('.h1');
-var Titles = ["WELCOME TO MY SITE", "ABOUT ME", "MY SKILLS", "MY WORK", "CONTACT"];
-
-for(var i=0; i<my_home.length; ++i){
-    if(i < Titles.length){
-        my_home[i].textContent = Titles[i];
-    }else{
-        my_home[i].textContent = "ERROR: Titles[] out of bound";
-    }
-}
